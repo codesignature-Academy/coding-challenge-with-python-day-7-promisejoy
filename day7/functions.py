37537f523fd818bce2cefe6dcece142b77492a08
SmartDevTechs <smartdevtechs@gmail.com>
2025-11-20 21:05:40 +0100

    submited

---
+++ day7/functions.py
@@ -0,0 +1,29 @@
+"""
+write four func that perform the following tasks:
+add (a,b): returns the sum of a and b
+multiply (a,b): returns  the product of a and b
+power (a,b): returns a raised to the power of b
+substract (a,b): returns the diffrence of a and b
+"""
+
+def add(a,b):
+    return a + b
+result = add(10, 5)
+print(result)
+
+def mult(a,b):
+    return a*b
+newresult = mult(15, 5)
+print(newresult)
+
+
+def power(a,b):
+    return a **b
+result_power= power(9, 3)
+print(result_power)
+
+
+def sub(a,b):
+    return a - b
+sub_sum = sub(566, 225)
+print(sub_sum)
